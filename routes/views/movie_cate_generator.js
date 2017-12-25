3658425d4a5689f90c9d091b6af2cdbe396c529c
DavidTsui <st880221@gmail.com>
2017-12-25 14:55:50 +0800

    修正影片分類controller問題，原本routes是送name，不知為何變成key，改成key就行啦!

--- routes/views/movie_cate_generator.js
+++ routes/views/movie_cate_generator.js
@@ -4,7 +4,7 @@ var co                      = require('co');
 var fix_keystone_pagination = require('../helpers/fix_keystone_pagination');
 var Movie                   = keystone.list('Movie');
 
-exports = module.exports = function(Category, cate_key_name, section, title) { 
+exports = module.exports = function(Category, cate_key_name, section, title) {
 	return function (req, res) {
 		var view = new keystone.View(req, res);
 		var locals = res.locals;
@@ -26,13 +26,12 @@ exports = module.exports = function(Category, cate_key_name, section, title) {
 					categories: Category.model.find().sort('-startDate').exec(),
 
 					// Load the current root_category filter
-					category: (locals.filters.category ? 
-						Category.model.where({name: locals.filters.category}) :
+					category: (locals.filters.category ?
+						Category.model.where({key: locals.filters.category}) :
 						Category.model).findOne().sort('-startDate').exec(),
 				};
-
-				for (var attrname in results) { 
-					locals[attrname] = results[attrname]; 
+				for (var attrname in results) {
+					locals[attrname] = results[attrname];
 				}
 
 				var movie_paginated_query = Movie.paginate({
@@ -53,7 +52,7 @@ exports = module.exports = function(Category, cate_key_name, section, title) {
 				results = yield {
 					// Load movies
 					movies: new Promise(function(resolve, reject){
-						movie_paginated_query.exec(function(err, ret) { 
+						movie_paginated_query.exec(function(err, ret) {
 							if (err) {
 								reject(err);
 							}	else {
@@ -65,13 +64,12 @@ exports = module.exports = function(Category, cate_key_name, section, title) {
 				};
 
 				locals.movies =	fix_keystone_pagination(results.movies,	results.movies_vanilla_result.length, 16);
-					
 			}).then(
-				function(){ 
-					callback(); 
+				function(){
+					callback();
 				},
-				function(e){ 
-					console.error(e, e.stack); callback(e); 
+				function(e){
+					console.error(e, e.stack); callback(e);
 				}
 			);
 		});
